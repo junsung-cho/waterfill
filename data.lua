e466bb3c6a291fe8c146871314b34418b1132143
Junsung Cho <97071544+junsung-cho@users.noreply.github.com>
2024-10-30 15:59:47 +0900

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,8 +1,3 @@
 require("prototypes.item")
 require("prototypes.recipe")
 require("prototypes.technology")
-
-if mods["space-age"] then
-    require("prototypes.item-space")
-    require("prototypes.recipe-space")
-end
